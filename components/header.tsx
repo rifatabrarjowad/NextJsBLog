68f0ec1b1a23893eb02af7cd10e685e89427bd9c
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-05 02:27:55 +0600

    heading linking

---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export default function header() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+        Hello Header!
+    </h1>
+    )
+    
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className={styles.container}>
+      <Header></Header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
