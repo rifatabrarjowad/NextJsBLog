a08c25ec540d547782ba850a5362ca59b0fe94a4
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-05 04:45:04 +0600

    section

--- components/section1.tsx
+++ components/section1.tsx
@@ -4,7 +4,7 @@ export default function section1() {
     return (
         <section className="py-16">
             <div className="container mx-auto md:px-20">
-            <h1 className="font-bold text-4xl pb-12 text-center">Section 1</h1>
+            <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
             {Slide()}
             </div>
         </section>
@@ -16,13 +16,22 @@ function Slide(){
         <div className="grid md:grid-cols-2">
             <div className="image">
                <Link className="" href="https://www.youtube.com/watch?v=1Q8fG0TtVAY">
-               <Image src="/images/img1.jpg" width="600" height="600" />
+               <Image src="/images/img1.jpg" width="500" height="500" />
                </Link>
             </div>
-            <div className="info">
+            <div className="info flex justify-center flex-col">
                 <div className="cat">
-                    <Link className="text-orange-600 hover:text-orange-800" href="/category/1">Business, Travel</Link>
+                    <Link href={"/"}  className="text-orange-600 hover:text-orange-800">Business, Travel</Link>
+                    <Link href={"/"} className="text-gray-800 hover:text-gray-600">- July 3, 2022</Link>
                 </div>
+                <div className="title">
+                    <Link href={"/"} className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
+                    text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+                </p>
+                <h1>author</h1>
             </div>
         </div>
     )
