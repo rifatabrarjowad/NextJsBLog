cfd127b2b29822b1f3873f6c28d4d22300c7f674
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-05 03:36:44 +0600

    nav

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,20 @@
+import { ImFacebook,ImTwitter,ImYoutube } from "react-icons/im";
 export default function header() {
     return (
         <header className="bg-gray-50 ">
             <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
                 <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-                    <input type="text" placeholder="Search.." className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-300 rounded-full text-sm shadow-sm placeholder-slate-500" />
+                    <input type="text" placeholder="Search.." className="input-text" />
 
                 </div>
                 <div className=" shrink w-80 sm:order-2">
-                    <a>Design</a>
+                    <a className="font-bold uppercase text-3xl" >Design</a>
                 </div>
                 <div className="w-96 order-3 flex justify-center">
                     <div className="flex gap-6">
-                        <a>Facebook</a>
-                        <a>Youtube</a>
-                        <a>Twitter</a>
+                        <a><ImFacebook color="#888888"/></a>
+                        <a><ImYoutube color="#888888"/></a>
+                        <a><ImTwitter color="#888888"/></a>
                     </div>
                 </div>
             </div>
