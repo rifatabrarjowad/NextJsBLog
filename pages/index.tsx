d20d296d4cf0e58a7e28f742e3cf335e0e7fe5a3
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-06 02:38:47 +0600

    let post

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Format from '../layout/format'
 import Section1 from '../components/section1'
 import Section2 from '../components/section2'
+import Section3 from '../components/section3'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Format>
         <Section1></Section1>
         <Section2></Section2>
+        <Section3></Section3>
       </Format>
    
   )
