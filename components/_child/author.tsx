6009019a54b374722876aa8089f82215cfe9a368
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-05 21:33:27 +0600

    author

---
+++ components/_child/author.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Image from 'next/image'
+export default function author() {
+    return (
+        <div className="author flex py-5">
+        <Image src={"/images/author/author1.jpg"} width={60} height={60} className="rounded-full"></Image>        
+        <div className="flex flex-col justify-center px-4">
+            <Link  className="text-md font-bold text-gray-800 hover:text-gray-600" href={"/"}>Flying High</Link>
+            <span className="text-sm text-gray-500">CEO and Founder</span>
+        </div>
+    </div>
+    )
+};
